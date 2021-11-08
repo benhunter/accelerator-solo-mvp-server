5bfc15964a6ba4f5ca9e9573dfcd5a13101504b1
Ben Hunter <learntocode@benhunter.me>
2021-11-08 16:22:07 -0600

    Initial setup

---
+++ src/main/java/com/benhunter/solomvpserver/SoloMvpServerApplication.java
@@ -0,0 +1,13 @@
+package com.benhunter.solomvpserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoloMvpServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoloMvpServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/benhunter/solomvpserver/SoloMvpServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.benhunter.solomvpserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoloMvpServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
