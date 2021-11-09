a7735550c0edb75f8702d12ee1d8a9746a0fbf32
Ben Hunter <code@benhunter.me>
2021-11-09 14:51:50 -0600

    Discord webhooks working.

--- src/main/java/com/benhunter/solomvpserver/AlarmChecker.java
+++ src/main/java/com/benhunter/solomvpserver/AlarmChecker.java
@@ -2,10 +2,17 @@ package com.benhunter.solomvpserver;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.MediaType;
+import org.springframework.http.RequestEntity;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
 
+import java.net.URI;
 import java.util.concurrent.Future;
 import java.util.concurrent.TimeUnit;
 
@@ -35,11 +42,21 @@ public class AlarmChecker {
             log.info("Checking alarm id: " + alarm.getId()
                     + " name: " + alarm.getName()
             );
+            checkSingleAlarm(alarm);
         }
     }
 
-//    @Async
-//    public Future<String> asyncChecker(Alarm alarm) {
-//        // TODO Check the target of alarm
-//    }
+
+
+    private final RestTemplate restTemplate = new RestTemplate();
+
+    public void checkSingleAlarm(Alarm alarm) {
+        String message = "{\"content\":\"Hello discord\"}";
+
+        // TODO send the messge to Discord only if alarm.target is down.
+        RequestEntity<String> request = RequestEntity.post(alarm.getWebhook())
+                .contentType(MediaType.APPLICATION_JSON)
+                .body(message);
+        restTemplate.exchange(request, Void.class);
+    }
 }
