6d0339488b13ccafc7ff9af60ade572cbcc5c5da
Ben Hunter <code@benhunter.me>
2021-11-09 17:09:19 -0600

    CORS for client.

--- src/main/java/com/benhunter/solomvpserver/AlarmRepository.java
+++ src/main/java/com/benhunter/solomvpserver/AlarmRepository.java
@@ -1,6 +1,8 @@
 package com.benhunter.solomvpserver;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
+@CrossOrigin("http://localhost:3000")
 public interface AlarmRepository extends CrudRepository<Alarm, Long> {
 }
