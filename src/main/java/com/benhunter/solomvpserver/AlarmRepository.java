76ffbb218584c4ddc73dba52f8b7a1210fd973e4
Ben Hunter <learntocode@benhunter.me>
2021-11-10 16:34:09 -0600

    Spring Security with explicit defaults (allowing all requests).

--- src/main/java/com/benhunter/solomvpserver/AlarmRepository.java
+++ src/main/java/com/benhunter/solomvpserver/AlarmRepository.java
@@ -1,8 +1,12 @@
 package com.benhunter.solomvpserver;
 
 import org.springframework.data.repository.CrudRepository;
+//import org.springframework.security.access.annotation.Secured;
+//import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
 @CrossOrigin("http://localhost:3000")
+//@PreAuthorize("hasRole('USER')")
+//@Secured("USER")
 public interface AlarmRepository extends CrudRepository<Alarm, Long> {
 }
---
+++ src/main/java/com/benhunter/solomvpserver/WebSecurityConfig.java
@@ -0,0 +1,35 @@
+package com.benhunter.solomvpserver;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+//        http.authorizeRequests((requests) -> requests.anyRequest().permitAll())  // allow any request.
+        http.authorizeRequests((requests) -> requests.anyRequest().authenticated())  // allow only authenticated requests.
+                .httpBasic(Customizer.withDefaults())
+                .formLogin(Customizer.withDefaults());
+    }
+
+    @Bean
+    public InMemoryUserDetailsManager userDetailsService() {
+        UserDetails user = User.withDefaultPasswordEncoder()
+                .username("user")
+                .password("password")
+                .roles("USER")
+                .build();
+        return new InMemoryUserDetailsManager(user);
+    }
+
+}
